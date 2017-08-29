1b20f2b0e350c92838b13df558cac2e0ceb8263e
diaphon <diaphon@users.noreply.github.com>
2017-08-29 13:44:53 +0200

    Update atbash.py

--- atbash.py
+++ atbash.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import string
+import string, re
 
 alphabet = list(string.ascii_uppercase)
 valpha   = alphabet[::-1]
 
 word = input("Type the word: ").upper()
+word = word.replace("Ä", "A")
+word = word.replace("Ö", "O")
+word = word.replace("Ü", "U")
+word = word.replace("ß", "S")
 
 for letter in word:
-  print("atbash for", letter, "=" , valpha[alphabet.index(letter)])
+  try:
+    print("atbash for", letter, "=" , valpha[alphabet.index(letter)])
+  except:
+    print("Error with input", letter)
