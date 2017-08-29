80e832a61a9bf07958649d11449f26e3d268e401
diaphon <diaphon@users.noreply.github.com>
2017-08-29 13:32:46 +0200

    Update and rename atbash to atbash.py

--- atbash.py
+++ atbash.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import string
+
 alphabet = list(string.ascii_uppercase)
 valpha   = alphabet[::-1]
+
 word = input("Type the word: ").upper()
 
 for letter in word:
